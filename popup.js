3017a652c2bb83b04d5191572dc8a497d0ce38b4
sbrand83 <brandsm@rose-hulman.edu>
2017-06-18 10:27:15 -0400

    can now remove tab groups

--- popup.js
+++ popup.js
@@ -1,4 +1,3 @@
-// Get all active tabs in the window
 var local_groups = [];
 
 var createGroupButton = document.getElementById("create-group-button");
@@ -60,21 +59,21 @@ function deserializeGroups(serializedGroups) {
 }
 
 function updateGroupsList() {
-    if (local_groups.length === 0) {
-        var noGroupsText = document.createElement("p");
-        var p_text = document.createTextNode("You don't have any groups yet. Add one below.");
-        noGroupsText.appendChild(p_text);
-        return;
-    }
-
     var groups_list = document.getElementById("groups-list");
     // clear the list
     while (groups_list.firstChild) {
         groups_list.removeChild(groups_list.firstChild);
     }
-    
-    for (var i = 0; i < local_groups.length; i++) {
-        addGroupToList(local_groups[i]);
+
+    if (local_groups.length === 0) {
+        var noGroupsText = document.createElement("p");
+        var p_text = document.createTextNode("You don't have any groups yet. Add one below.");
+        noGroupsText.appendChild(p_text);
+        groups_list.appendChild(noGroupsText);
+    } else {
+        for (var i = 0; i < local_groups.length; i++) {
+            addGroupToList(local_groups[i]);
+        }
     }
 }
 
@@ -83,15 +82,19 @@ function addGroupToList(group) {
     var group_button = document.createElement("button");
     var button_text = document.createTextNode(group.getName());
     group_button.appendChild(button_text);
-    
     group_button.addEventListener("click", openGroup);
 
+    var removeGroupButton = document.createElement("button");
+    var removeGroupButtonText = document.createTextNode("Remove");
+    removeGroupButton.appendChild(removeGroupButtonText);
+    removeGroupButton.value = group.getName();
+    removeGroupButton.addEventListener("click", removeGroup);
+
     groups_list.appendChild(group_button);
-    console.log("creating new group");
+    groups_list.appendChild(removeGroupButton);
 }
 
 function openGroup(event) {
-    console.log("opening group");
     var groupName = event.srcElement.textContent;
     var selectedGroup;
 
@@ -108,4 +111,26 @@ function openGroup(event) {
 
     var tab_urls = selectedGroup.getTabUrls();
     chrome.windows.create({url: tab_urls});
+}
+
+function removeGroup(event) {
+    var groupName = event.srcElement.value;
+
+    chrome.storage.sync.get('groups', function(items) {
+        var serialized_groups = items.groups;
+        if (serialized_groups) {
+            for (var i = 0; i < serialized_groups.length; i++) {
+                if (serialized_groups[i].name === groupName) {
+                    //remove the group if it is there
+                    serialized_groups.splice(i, 1);
+                    chrome.storage.sync.set({"groups": serialized_groups});
+                }
+            }
+        } else {
+            serialized_groups = [];
+        }
+
+        deserializeGroups(serialized_groups);
+        updateGroupsList();
+    });
 }
